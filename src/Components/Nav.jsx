3276bd258cea0df4f8798ac9608be82945666ac1
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-30 09:57:55 -0400

    Add hero component desktop css

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import logo from "../Assets/images/logo.svg"
 import hamburger from "../Assets/images/icon-hamburger.svg"
+import arrow from "../Assets/images/icon-arrow-light.svg"
+
+import "../Assets/Styles/Nav/Nav.css"
 
 function Nav(){
+
+    const links = ["Product", "Company", "Connect"]
+
     return(
         <nav id="main-nav">
-            <img className="logo" src={logo} alt="logo" />
-            <img className="menu" src={hamburger} alt="menu" />
+            <div className="inner-nav">
+                <img className="logo" src={logo} alt="logo" />
+
+                {
+                    links.map(item => {
+                        return(
+                            <div className="inner-nav inner-nav--links desktop">
+                                <span>{item}</span>
+                                <img className="arrow" src={arrow} alt="arrow" />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className='inner-nav'>
+                <img className="menu mobile" src={hamburger} alt="menu" />
+                <span className="desktop">Login</span>
+                <button className="main-btn desktop">Sign Up</button>
+            </div>
         </nav>
     )
 }
