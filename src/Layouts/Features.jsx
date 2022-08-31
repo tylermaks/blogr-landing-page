f7c362ccbb419b8e43b68284901079f965bc43bf
Tyler Maksymiw <tylermcodes@gmail.com>
2022-08-31 10:08:44 -0400

    Update feature component and add css

--- src/Layouts/Features.jsx
+++ src/Layouts/Features.jsx
@@ -1,24 +1,32 @@
 import React from "react"
 import featureData from "../FeatureData";
 
+import "../Assets/Styles/Features/Features.css"
+
 function Features({instance}){
     
     return(
         <>  
             {
                 featureData.slice(instance, (instance + 1)).map(item => {
+
+                    const rowDirection = instance === 0 ? "right" : "left"
+
                     return(
-                        <section>
+                        <section className="features">
                             <h2>{item.title}</h2>
-                            <img src={item.mobileImg} alt="Editor" />
-                            <article>
-                                <h3>{item.subHeadingOne}</h3>
-                                <p>{item.paragraphOne}</p>
-                            </article>
-                            <article>
-                                <h3>{item.subHeadingTwo}</h3>
-                                <p>{item.paragraphTwo}</p>
-                            </article>
+                            <div className={"features-inner features-inner--" + rowDirection}>
+                                <div>
+                                    <img className="mobile-img" src={item.mobileImg} alt="Editor" />
+                                    <img className={"desktop-img desktop-img--" + rowDirection} src={item.desktopImg} alt="Editor" />
+                                </div>
+                                <article>
+                                    <h2>{item.subheadingOne}</h2>
+                                    <p>{item.paragraphOne}</p>
+                                    <h2>{item.subheadingTwo}</h2>
+                                    <p>{item.paragraphTwo}</p>
+                                </article>
+                            </div>
                         </section>
                     )
              
